86ab30b1159abfd28cf5d0653a1ec4b3cf992fdc
Basidh Nizam <basidhn20@gmail.com>
2026-03-05 18:59:50 +0530

    implementation of user profiles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Profile from "./pages/profile/Profile";
+import Addresses from "./pages/profile/Addresses";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -13,6 +16,10 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+
+        {/* DEV 2: Profile & Address Management */}
+        <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="profile/addresses" element={<ProtectedRoute><Addresses /></ProtectedRoute>} />
 
         {/* DEV 3 will add: */}
         {/* <Route path="products" element={<ProductList />} /> */}
