289c9aba139fc33134abf74a3056f5b0f41b84be
Basidh Nizam <basidhn20@gmail.com>
2026-03-05 17:38:33 +0530

    implementation of login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -9,9 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* DEV 2 will add: */}
-        {/* <Route path="login" element={<Login />} /> */}
-        {/* <Route path="register" element={<Register />} /> */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
         {/* DEV 3 will add: */}
         {/* <Route path="products" element={<ProductList />} /> */}
