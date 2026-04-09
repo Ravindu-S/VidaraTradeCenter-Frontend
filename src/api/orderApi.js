2d3315ea27326eadf97290a22fc439fb013f5a13
NisalWeerakoon <it24103467@my.sliit.lk>
2026-04-09 14:01:22 +0530

    fixed merged conflicts

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -4,6 +4,10 @@ export const getOrderByNumber = (orderNumber) => {
   return API.get(`/orders/${orderNumber}`);
 };
 
+export const getDeliveryStatus = (orderNumber) => {
+  return API.get(`/orders/${orderNumber}/delivery-status`);
+};
+
 export const getMyOrders = (page = 0, size = 10) => {
   return API.get(`/orders`, { params: { page, size } });
 };
